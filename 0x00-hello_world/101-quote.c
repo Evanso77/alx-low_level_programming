ea28081f5319c582b78f0cabb989f77bc3ef1528
Evanso77 <125613894+Evanso77@users.noreply.github.com>
2023-04-06 14:13:14 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-#include <unistd.h>
-/**
- * main - prints exactly and that piece of art is useful
- * Return: 1 (Success)
- */
+
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
